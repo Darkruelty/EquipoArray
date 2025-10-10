bb98cdbcfe2abee0b6b7f893a5a5c620017cad62
Darkruelty <darkruelty@gmail.com>
2025-10-10 15:53:54 -0600

    Ejercicios101025

---
+++ Ejercicio.py
@@ -0,0 +1,16 @@
+#1.Desarrolla un programa que cumpla con lo siguiente:
+#Solicite al usuario el tamaño del arreglo (la cantidad de elementos que tendrá).
+#Solicite al usuario un número entero.
+#Llene el arreglo con los múltiplos de ese número, comenzando desde el primero.
+#Ejemplo: si el tamaño es 5 y el número ingresado es 3, el arreglo deberá contener: 3, 6, 9, 12, 15.
+#Muestre en pantalla todos los elementos del arreglo.
+
+#2.Desarrolla un programa que cumpla con lo siguiente:
+#Pida al usuario el tamaño de los arreglos (la cantidad de nombres que se van a guardar).
+#Cree un primer arreglo para guardar nombres de personas (como texto).
+#Cree un segundo arreglo donde se guarde la cantidad de letras que tiene cada nombre (la longitud).
+#Finalmente, muestra en pantalla ambos arreglos: los nombres y su respectiva longitud.
+#Ejemplo:
+#Tamaño: 3
+#Nombres: ["Ana", "Roberto", "Luis"]
+#Longitudes: [3, 7, 4]
